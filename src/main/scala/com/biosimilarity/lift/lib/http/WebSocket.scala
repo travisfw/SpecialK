63d866dd3f0bcef342f0aebde7bfc136eb49c31a
Meredith Gregory <lgreg.meredith@biosimilarity.com>
2012-01-07 09:59:26 -0800

    Added kvdb instantiation

--- src/main/scala/com/biosimilarity/lift/lib/http/WebSocket.scala
+++ src/main/scala/com/biosimilarity/lift/lib/http/WebSocket.scala
@@ -99,11 +99,23 @@ package usage {
  * ------------------------------------------------------------------ */ 
 
   import com.biosimilarity.lift.model.store._
-  import com.biosimilarity.lift.model.store.usage._
-  import com.biosimilarity.lift.model.store.usage.PersistedMonadicTS._
+  import com.biosimilarity.lift.model.agent._
+  import com.biosimilarity.lift.model.msg._
+
+  import com.biosimilarity.lift.lib.UUIDOps
+  import com.biosimilarity.lift.lib.moniker._
 
   import com.biosimilarity.magritte.json._
   import com.biosimilarity.magritte.json.Absyn._
+
+  import scala.xml._
+  import scala.collection.MapProxy
+  import scala.collection.mutable.Map
+  import scala.collection.mutable.HashMap
+  import scala.collection.mutable.LinkedHashMap
+  import scala.collection.mutable.ListBuffer
+  import scala.collection.mutable.MutableList
+
   import java.io.StringReader
 
   trait Argonaut {
@@ -119,5 +131,176 @@ package usage {
 	}
       }
   }   
+
+  object WebsocketMonadicTS
+     extends PersistedTermStoreScope[String,String,String,String] 
+     with UUIDOps
+  {
+    import com.biosimilarity.lift.lib.SpecialKURIDefaults._
+    import com.biosimilarity.lift.model.store.CnxnLeafAndBranch._
+    import identityConversions._
+
+    type MTTypes = MonadicTermTypes[String,String,String,String]
+    object TheMTT extends MTTypes
+    override def protoTermTypes : MTTypes = TheMTT
+
+    type DATypes = DistributedAskTypes
+    object TheDAT extends DATypes
+    override def protoAskTypes : DATypes = TheDAT
+    
+    class WebsocketStringMGJ(
+      val dfStoreUnitStr : String,
+      //override val name : URI,
+      override val name : Moniker,
+      //override val acquaintances : Seq[URI]
+      override val acquaintances : Seq[Moniker]
+    ) extends PersistedMonadicGeneratorJunction(
+      name, acquaintances
+    ) {
+      class StringXMLDBManifest(
+	override val storeUnitStr : String,
+	override val labelToNS : Option[String => String],
+	override val textToVar : Option[String => String],
+	override val textToTag : Option[String => String]        
+      )
+      extends XMLDBManifest( database ) {
+	override def storeUnitStr[Src,Label,Trgt](
+	  cnxn : Cnxn[Src,Label,Trgt]
+	) : String = {     
+	  cnxn match {
+	    case CCnxn( s, l, t ) =>
+	      s.toString + l.toString + t.toString
+	  }
+	}	
+
+	def kvNameSpace : String = "record"
+
+	// BUGBUG -- LGM: Evidence of a problem with this factorization
+	override def asCacheValue(
+	  ltns : String => String,
+	  ttv : String => String,
+	  value : Elem
+	) : Option[String] = {
+	  tweet(
+	    "Shouldn't be here!"
+	  )
+	  None
+	}
+
+	override def asStoreValue(
+	  rsrc : mTT.Resource
+	) : CnxnCtxtLeaf[String,String,String] with Factual = {
+	  valueStorageType match {
+	    case "CnxnCtxtLabel" => {
+	      tweet(
+		"warning: CnxnCtxtLabel method is using XStream"
+	      )
+
+	      val blob = toXQSafeJSONBlob( rsrc )
+
+	      new CnxnCtxtLeaf[String,String,String](
+		Left[String,String](
+		  blob
+		)
+	      )
+	    }
+	    case "XStream" => {
+	      tweet(
+		"using XStream method"
+	      )
+
+	      val blob = toXQSafeJSONBlob( rsrc )
+
+	      //asXML( rsrc )
+	      new CnxnCtxtLeaf[String,String,String](
+		Left[String,String]( blob )
+	      )
+	    }
+	    case _ => {
+	      throw new Exception( "unexpected value storage type" )
+	    }
+	  }	  
+	}
+
+	def asCacheValue(
+	  ccl : CnxnCtxtLabel[String,String,String]
+	) : String = {
+	  tweet(
+	    "converting to cache value"
+	  )
+	  //asPatternString( ccl )
+	  ccl match {
+	    case CnxnCtxtBranch(
+	      "String",
+	      CnxnCtxtLeaf( Left( rv ) ) :: Nil
+	    ) => {
+	      val unBlob =
+		fromXQSafeJSONBlob( rv )
+
+	      unBlob match {
+		case rsrc : mTT.Resource => {
+		  getGV( rsrc ).getOrElse( "" )
+		}
+	      }
+	    }
+	    case _ => {
+	      asPatternString( ccl )
+	    }
+	  }
+	}
+      
+      }
+
+      def persistenceManifest : Option[PersistenceManifest] = {
+	val sid = Some( ( s : String ) => s )
+	Some(
+	  new StringXMLDBManifest( dfStoreUnitStr, sid, sid, sid )
+	)
+      }
+    }
+    
+    def ptToPt( storeUnitStr : String, a : String, b : String )  = {
+      new WebsocketStringMGJ( storeUnitStr, a, List( b ) )
+    }
+
+    def loopBack( storeUnitStr : String ) = {
+      ptToPt( storeUnitStr, "localhost", "localhost" )
+    }
+
+    import scala.collection.immutable.IndexedSeq
+        
+    type MsgTypes = DTSMSH[String,String,String,String]   
+    
+    val protoDreqUUID = getUUID()
+    val protoDrspUUID = getUUID()    
+    
+    object MonadicDMsgs extends MsgTypes {
+      
+      override def protoDreq : DReq = MDGetRequest( aLabel )
+      override def protoDrsp : DRsp = MDGetResponse( aLabel, aLabel.toString )
+      override def protoJtsreq : JTSReq =
+	JustifiedRequest(
+	  protoDreqUUID,
+	  new URI( "agent", protoDreqUUID.toString, "/invitation", "" ),
+	  new URI( "agent", protoDreqUUID.toString, "/invitation", "" ),
+	  getUUID(),
+	  protoDreq,
+	  None
+	)
+      override def protoJtsrsp : JTSRsp = 
+	JustifiedResponse(
+	  protoDreqUUID,
+	  new URI( "agent", protoDrspUUID.toString, "/invitation", "" ),
+	  new URI( "agent", protoDrspUUID.toString, "/invitation", "" ),
+	  getUUID(),
+	  protoDrsp,
+	  None
+	)
+      override def protoJtsreqorrsp : JTSReqOrRsp =
+	Left( protoJtsreq )
+    }
+    
+    override def protoMsgs : MsgTypes = MonadicDMsgs
+  }
 
 }
